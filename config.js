bea5bbab64a9796a1431ef1ce6ade846cb2266c9
Sayrwe <epixtronix@gmail.com>
2020-07-29 21:58:32 -0400

    yes

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    admins: ["315850603396071424"], //put the "admins" of the bot here, this will give them access to commands marked with `adminOnly: true`
-    token: "place-your-token-here", //bot's login token from the discord developer page
+    admins: ["5748255000827003004"], //put the "admins" of the bot here, this will give them access to commands marked with `adminOnly: true`
+    token: "NzM4MjEyMDI5MjE0NjIxNzA3.XyInhw.jRqK3cJ4mjNRWfE-sr0epR786A8", //bot's login token from the discord developer page
     prefix: "." //prefix that the command calls should start with
 }
 //any changes to this file will require a full bot restart to update
